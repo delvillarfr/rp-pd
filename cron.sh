959134f6b5f73ea8786c5569b85e7be1be94adb3
iorch <j.martinezortega@gmail.com>
2016-05-26 14:25:54 -0500

    correctin header

--- cron.sh
+++ cron.sh
@@ -8,22 +8,22 @@ RDATA=${WORKINGDIR}'/data'
 RSCRIPTS='/Users/iorch/mxabierto/rp-pd'
 
 if [ -f ${CURRENT_DATA}/contacts.csv ]; then
-c_date=`head -2 ${CURRENT_DATA}/contacts.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]"){print i} } }'|tail -1`
+c_date=`head -2 ${CURRENT_DATA}/contacts.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]-"){print i} } }'|tail -1`
 AFTER_CONTACTS=`head -2 ${CURRENT_DATA}/contacts.csv | awk -F, -v mon="$c_date" '{print $mon}' | tail -n 1`
 echo ${AFTER_CONTACTS}
 fi
 if [ -f ${CURRENT_DATA}/messages.csv ]; then
-m_date=`head -2 ${CURRENT_DATA}/messages.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]"){print i} } }'|tail -1`
+m_date=`head -2 ${CURRENT_DATA}/messages.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]-"){print i} } }'|tail -1`
 AFTER_MESSAGES=`head -2 ${CURRENT_DATA}/messages.csv | awk -F, -v mon="$m_date" '{print $mon}' | tail -n 1`
 echo ${AFTER_MESSAGES}
 fi
 if [ -f ${CURRENT_DATA}/flows.csv ]; then
-f_date=`head -2 ${CURRENT_DATA}/flows.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]"){print i} } }'|tail -1`
+f_date=`head -2 ${CURRENT_DATA}/flows.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]-"){print i} } }'|tail -1`
 AFTER_FLOWS=`head -2 ${CURRENT_DATA}/flows.csv | awk -F, -v mon="$f_date" '{print $mon}' | tail -n 1`
 echo ${AFTER_FLOWS}
 fi
 if [ -f ${CURRENT_DATA}/runs.csv ]; then
-r_date=`head -2 ${CURRENT_DATA}/runs.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]"){print i} } }'|tail -1`
+r_date=`head -2 ${CURRENT_DATA}/runs.csv|awk -F, '{for(i=1;i<=NF;i++){ if($i~"201[6-9]-"){print i} } }'|tail -1`
 AFTER_RUNS=`head -2 ${CURRENT_DATA}/runs.csv|awk -F, -v mon="$r_date" '{print $mon}'|tail -n 1`
 echo ${AFTER_RUNS}
 fi
@@ -38,7 +38,7 @@ else
 LINES_CONTACTS=$(( `wc -l < ${CURRENT_DATA}/contacts.csv`-1 ))
 cp ${DOWNLOADED}/contacts.csv ${TEMPORARY}/contacts.csv
 cat ${CURRENT_DATA}/contacts.csv | tail -n ${LINES_CONTACTS} >> ${TEMPORARY}/contacts.csv
-head -1 ${CURRENT_DATA}/contacts.csv > ${CURRENT_DATA}/contacts.csv
+head -1 ${TEMPORARY}/contacts.csv > ${CURRENT_DATA}/contacts.csv
 tail -n+2 ${TEMPORARY}/contacts.csv | sort -t, -k${c_date} -ur >> ${CURRENT_DATA}/contacts.csv
 fi
 echo 'contacs done'
@@ -49,7 +49,7 @@ else
 LINES_MESSAGES=$(( `wc -l < ${CURRENT_DATA}/messages.csv`-1 ))
 cp ${DOWNLOADED}/messages.csv ${TEMPORARY}/messages.csv
 cat ${CURRENT_DATA}/messages.csv | tail -n ${LINES_MESSAGES} >> ${TEMPORARY}/messages.csv
-head -1 ${CURRENT_DATA}/messages.csv > ${CURRENT_DATA}/messages.csv
+head -1 ${TEMPORARY}/messages.csv > ${CURRENT_DATA}/messages.csv
 tail -n+2 ${TEMPORARY}/messages.csv | sort -t, -k${m_date} -ur  >> ${CURRENT_DATA}/messages.csv
 fi
 echo 'messages done'
@@ -60,7 +60,7 @@ else
 LINES_FLOWS=$(( `wc -l < ${CURRENT_DATA}/flows.csv`-1 ))
 cp ${DOWNLOADED}/flows.csv ${TEMPORARY}/flows.csv
 cat ${CURRENT_DATA}/flows.csv | tail -n ${LINES_FLOWS} >> ${TEMPORARY}/flows.csv
-head -1 ${CURRENT_DATA}/flows.csv > ${CURRENT_DATA}/flows.csv
+head -1 ${TEMPORARY}/flows.csv > ${CURRENT_DATA}/flows.csv
 tail -n+2 ${TEMPORARY}/flows.csv | sort -t, -k${f_date} -ur >> ${CURRENT_DATA}/flows.csv
 fi
 echo 'flows done'
@@ -71,7 +71,7 @@ else
 LINES_RUNS=$(( `wc -l < ${CURRENT_DATA}/runs.csv`-1 ))
 cp ${DOWNLOADED}/runs.csv ${TEMPORARY}/runs.csv
 cat ${CURRENT_DATA}/runs.csv | tail -n ${LINES_FLOWS} >> ${TEMPORARY}/runs.csv
-head -1 ${CURRENT_DATA}/runs.csv > ${CURRENT_DATA}/runs.csv
+head -1 ${TEMPORARY}/runs.csv > ${CURRENT_DATA}/runs.csv
 tail -n+2 ${TEMPORARY}/runs.csv | sort -t, -k${r_date}  -ur >> ${CURRENT_DATA}/runs.csv
 fi
 echo 'runs done'
